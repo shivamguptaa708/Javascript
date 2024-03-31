159218c751af0b4e9f1222d5b94d924aaf35f43a
shivamguptaa708 <165505550+shivamguptaa708@users.noreply.github.com>
2024-03-31 11:06:04 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,5 @@ let accountEmail = "shivam@gmail.com"
 var accountPassword = "12345"
 accountcity = "nashik"
 
+
 console.table([accountId,accountEmail,accountPassword,accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+
+let name ="shivam"
+let age = 19
+let loggedIn = false
+let state;
+
+//datatypes:
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique 
+
+//object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof loggedIn)
+
+console.log(typeof state) // undefined
+
+console.log(typeof null) // object
+
+console.log(typeof undefined) // undefined
+ 
