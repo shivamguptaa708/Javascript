57043c7dfe4a8bd104e9aa2cea249b677ac6366f
shivamguptaa708 <165505550+shivamguptaa708@users.noreply.github.com>
2024-03-31 05:20:37 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144455
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "12345"
+accountcity = "nashik"
+
+console.table([accountId,accountEmail,accountPassword,accountcity])
